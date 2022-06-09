a4084d7836f750b43488a1c338c4eb8b3d9fc5d2
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2022-06-09 22:08:13 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, {useState } from 'react';
+import ReactDOM from 'react-dom'
+import Nav from './components/comp'
+import img from './assets/img.jpg'
+import './index.css'
+
+function Header() {
+  
+}
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <NavBar/>
+)
