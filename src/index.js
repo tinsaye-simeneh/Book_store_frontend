8500efa32dd3220d7b95a171f4e866c4d13dd6bc
eyos <eyosiastamirat20@gmail.com>
2022-06-09 23:17:44 +0300

    fixed index js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, {useState } from 'react';
 import ReactDOM from 'react-dom'
-import Nav from './components/comp'
-import img from './assets/img.jpg'
 import './index.css'
 
-function Header() {
-  
+function Book() {
+  return <>
+  Book app
+  </>
 }
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <NavBar/>
+  <Book/>
 )
