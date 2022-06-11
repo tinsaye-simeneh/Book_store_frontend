bd594e739d8c69693f26eff0625c312d02faba51
eyos <eyosiastamirat20@gmail.com>
2022-06-11 23:17:14 +0300

    import mistake

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,13 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { hover } from "@testing-library/user-event/dist/hover";
+
 //color #103037
 const useStyles = makeStyles((theme) => ({
   footer: {
-    /* position: "absolute",
+   /* position: "absolute",
     bottom: 0,
     left: 0,
-    right: 0,
-    */
+    right: 0, */
+    
     margin: "auto",
   },
   pagination: {},
