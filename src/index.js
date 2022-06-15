6de7e9f566f38ce41c50f700c358a5d22a498480
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2022-06-15 22:30:05 +0300

    pull

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 function Book() {
-<<<<<<< HEAD
 
   return (
   <>
 
-=======
-  return <>
-  <Header/>
-  <Home />
-  
-  <Footer />
->>>>>>> 09e636dbf76ab36b73bfad0370000cb512082a4c
   </>
   )
 }
